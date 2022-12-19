51056a84ceb014f3907d4a23078b3a98ecb4b1e9
ashwinramuk <46064775+ashwinramuk@users.noreply.github.com>
2022-12-19 22:44:40 +0530

    Update UserModel.js
    Replaced with camelCase for fieldnames.

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -3,13 +3,13 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const UserSchema = new Schema({    
-    Email: {type : String,unique:true, required: true},
-    Password: {type : String, required: true},
+    email: {type : String,unique:true, required: true},
+    password: {type : String, required: true},
 
     // NOT COMPLITED 
-    UserPicURL: String, //static one image	
+    userPicURL: String, //static one image	
     name: String, //	demo1	
     AccessDesignation: {type :String , required: true}	//normal, Admin, superAdmin, 
   })
-  const UserModel = mongoose.model('<name of the collection for User >', UserSchema ); // NOT COMPLITED 
+  const UserModel = mongoose.model('users', UserSchema ); // NOT COMPLITED 
   module.exports = UserModel;
