6a894b973569d83736e8fd3ee50084795d818310
hirak-gitv2 <royhirakp@gmail.com>
2022-12-21 17:56:17 +0530

    get method for search by email and method for getting contacts

--- src/routes/ContactRoute.js
+++ src/routes/ContactRoute.js
@@ -16,9 +16,7 @@ const storage = multer.diskStorage({
 })
 const uploads = multer({storage: storage})
 
-router.post('/<path>',(req,res)=>{
 
-})
 let contactResponse
 router.post('/',uploads.single('file'), (req,res)=>{
     req.userID="63a1f6c67fede683c41d1ee7"; //after login and JWT creation,delete this and get userID via JWT
@@ -35,4 +33,44 @@ router.post('/',uploads.single('file'), (req,res)=>{
         })
     })
 })
+
+router.get('/',async (req,res)=>{
+    try {
+      // console.log(req.query)
+      const {PageNum ,filter } = req.query
+      const allcontact = await ContactModel.find().sort(filter).skip((11*(PageNum - 1))).limit(11); //0-11
+      // console.log(allcontact)
+      res.json({
+          status: 'susecess',  
+          allcontact
+      })
+    } catch (error) {
+      res.json({
+          status: 'failed',
+          messege: error.messege
+      })
+    }
+  })
+
+//get the search contact
+router.get('/search/:email',async (req,res)=>{
+    try {
+      const email = req.params.email
+      console.log(email)
+      const allcontact = await ContactModel.find({email})
+      res.json({
+          status: 'susecess',  
+          allcontact
+      })
+    } catch (error) {       
+      res.json({
+          status: 'failed',
+          messege: error.messege
+      })
+    }
+  })
+
+  //
+
+
 module.exports = router;
